299bb33760f3ad1fbf1392460736bcae59ad3d8f
Naveen Pantra <naveenpantra.np@gmail.com>
2019-02-21 15:08:29 +0530

    Finished with comments and put \user request and done.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
+// Dependencies
 const server = require("./lib/server");
+
+
 const app = {};
 
 app.init = () => {
+
+    // Initiliazing server in ./lib/server
   server.init()
 };
+
 app.init();
 
 module.exports = app;
